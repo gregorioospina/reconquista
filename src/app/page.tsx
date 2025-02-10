08934036263f6179012f00403666a5f3fb95d3aa
gregorio_ospina <gregorio.ospina@rappi.com>
2025-02-10 16:42:04 -0500

    va

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<div
 			id="scrolling-container"
-			className="items-center justify-items-center min-h-screen gap-16 p-20 md:p-4 md:pt-14 font-[family-name:var(--font-geist-sans)]">
+			className="items-center justify-items-center min-h-screen gap-16 p-20 pb-8 overflow-hidden md:p-4 md:pt-14 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start px-5 md:px-[25svw]">
 				<div className="h-[70svh] relative max-w-[500px] w-[90svw] md:w-[80svw]">
 					<p className="mx-5 relative font-title text-8xl md:text-9xl mt-5 z-10 whitespace-break-spaces text-background">
