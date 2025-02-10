42fb5136f82659f4dba3d7a2137a9dfa8cb22e26
gregorio_ospina <gregorio.ospina@rappi.com>
2025-02-10 14:48:04 -0500

    va

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useMemo, useState } from "react";
 import Menu from "./menu";
 
@@ -28,9 +29,11 @@ const Header = (props: IHeader) => {
 
 	return (
 		<div className="fixed top-0 left-0 right-0 border-b p-3 md:p-3 md:pr-10 pr-5 pl-5 md:pl-10 border-gray-100 z-50 bg-background md:bg-transparent md:border-none flex justify-between">
-			<p className="font-title text-lg md:text-2xl">
-				MC<span className={[andColor, "mx-2 font-bold"].join(" ")}>&</span>A
-			</p>
+			<Link href={"/"}>
+				<p className="font-title text-lg md:text-2xl">
+					MC<span className={[andColor, "mx-2 font-bold"].join(" ")}>&</span>A
+				</p>
+			</Link>
 			{props.menu && (
 				<button onClick={() => setOpenMenu(true)}>
 					<Image src={"./assets/images/menu-icon.svg"} alt="menu icon" height={20} width={20} />
