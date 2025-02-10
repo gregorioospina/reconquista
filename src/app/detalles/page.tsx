1a2c4c4a75f02ea7e33dc2e600cb471d423180fa
gregorio_ospina <gregorio.ospina@rappi.com>
2025-02-10 16:35:59 -0500

    listo pa

--- src/app/detalles/page.tsx
+++ src/app/detalles/page.tsx
@@ -13,9 +13,28 @@ export default function Details() {
 				<div className="flex flex-col justify-start">
 					<Title lineSrc="./assets/images/line-g.svg" yOffset={-17} title="Detalles" />
 				</div>
-				{/* todo */}
-				<div className="h-[60svh] relative w-full mt-[-120px] mb-[-90px] md:mb-[-90px]">
+				<div className="h-[60svh] relative w-full mt-[-120px] mb-[-90px] md:mb-[-100px]">
 					<Image src={"/assets/images/wed.png"} alt="detalles" fill className="object-contain" />
+				</div>
+				<div className="flex flex-col z-10">
+					<p className="uppercase font-body font-light text-xs md:text-sm">Sábado 2 de agosto</p>
+					<p className="font-title text-3xl md:text-4xl">Ceremonia y Celebración</p>
+					<p className="font-body font-semibold text-sm md:text-base mt-2">Finca Haras Primavera Sopó</p>
+					<p className="font-body md:text-base md:leading-5 leading-4 text-sm  text-justify">
+						Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over
+						2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
+						consectetur, from a Lorem Ipsum passage
+					</p>
+					<p className="font-body font-semibold text-sm md:text-base mt-1">Vestido Largo y Traje</p>
+				</div>
+
+				<div className="mt-1 flex flex-col gap-y-3 items-start">
+					<button className="underline text-sm md:text-base font-highlights font-light">confirmar</button>
+					<button className="underline text-sm md:text-base font-highlights font-light">¿cómo llegar?</button>
+					<button className="underline text-sm md:text-base font-highlights font-light">necesito transporte</button>
+				</div>
+				<div className="w-full h-8 relative my-4">
+					<Image src={"/assets/images/divider-3.svg"} fill alt="divider 1 " />
 				</div>
 				<div className="flex flex-col">
 					<p className="uppercase font-body font-light text-xs md:text-sm">Jueves 31 de agosto</p>
@@ -29,30 +48,13 @@ export default function Details() {
 					<p className="font-body font-semibold text-sm md:text-base mt-1">Be smart, be casual</p>
 				</div>
 
-				<div className="mt-1 flex flex-col gap-y-3 items-start">
-					<button className="underline text-sm md:text-base font-highlights font-light">confirmar</button>
-					<button className="underline text-sm md:text-base font-highlights font-light">¿cómo llegar?</button>
-					<button className="underline text-sm md:text-base font-highlights font-light">necesito transporte</button>
-				</div>
-				<div className="w-full h-8 relative my-4">
-					<Image src={"/assets/images/divider-3.svg"} fill alt="divider 1 " />
-				</div>
-				<div className="flex flex-col">
-					<p className="uppercase font-body font-light text-xs md:text-sm">Sábado 2 de agosto</p>
-					<p className="font-title text-3xl md:text-4xl">Ceremonia y Celebración</p>
-					<p className="font-body font-semibold text-sm md:text-base mt-2">Finca Haras Primavera Sopó</p>
-					<p className="font-body md:text-base md:leading-5 leading-4 text-sm  text-justify">
-						Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over
-						2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-						consectetur, from a Lorem Ipsum passage
-					</p>
-					<p className="font-body font-semibold text-sm md:text-base mt-1">Vestido Largo y Traje</p>
-				</div>
-
-				<div className="mt-1 flex flex-col gap-y-3 items-start">
+				<div className="mt-1 z-10 flex flex-col gap-y-3 items-start">
 					<button className="underline font-highlights text-sm md:text-base font-light">confirmar</button>
 					<button className="underline font-highlights text-sm md:text-base font-light">¿cómo llegar?</button>
 					<button className="underline font-highlights text-sm md:text-base font-light">necesito transporte</button>
+				</div>
+				<div className="h-[60svh] mt-[-130px] relative w-full ">
+					<Image src={"/assets/images/picoteo-1.png"} alt="detalles" fill className="object-contain" />
 				</div>
 			</main>
 		</div>
