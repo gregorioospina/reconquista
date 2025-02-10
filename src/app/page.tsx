9c6d9402125fd3ace8a9b182eddf2aa582f7c275
gregorio_ospina <gregorio.ospina@rappi.com>
2025-02-10 16:39:58 -0500

    dios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<div
 			id="scrolling-container"
-			className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			className="items-center justify-items-center min-h-screen gap-16 p-20 md:p-4 md:pt-14 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start px-5 md:px-[25svw]">
 				<div className="h-[70svh] relative max-w-[500px] w-[90svw] md:w-[80svw]">
 					<p className="mx-5 relative font-title text-8xl md:text-9xl mt-5 z-10 whitespace-break-spaces text-background">
@@ -17,7 +17,7 @@ export default function Home() {
 					</p>
 					<Image className="object-fill z-[9]" src={"/assets/images/cover.png"} fill alt="cover"></Image>
 				</div>
-				<div className="flex justify-center w-full">
+				<div className="flex justify-center w-full h-20">
 					<button onClick={() => setOpenMenu(true)} className="text-sm underline">
 						ver menú
 					</button>
