d7031f318ad2c4cefe39affada4771e779959d5d
gregorio_ospina <gregorio.ospina@rappi.com>
2025-02-13 05:09:56 -0500

    ajustes

--- src/app/detalles/page.tsx
+++ src/app/detalles/page.tsx
@@ -14,56 +14,56 @@ export default function Details() {
 				<div className="flex flex-col justify-start">
 					<Title lineSrc="./assets/images/line-g.svg" yOffset={-17} title="Detalles" />
 				</div>
-				<div className="h-[60svh] relative w-full mt-[-120px] mb-[-100px] md:mb-[-80px]">
-					<Image src={"/assets/images/wed.png"} alt="detalles" fill className="object-contain" />
+				<div className="h-[40svh] md:h-[50svh] relative w-full mt-[-20px] mb-[-50px]">
+					<Image src={"/assets/images/picoteo-1.png"} alt="detalles" fill className="object-contain" />
 				</div>
+
+				<div className="flex flex-col">
+					<p className="uppercase font-body font-light text-xs md:text-sm">Jueves 31 de agosto, 5:30 pm</p>
+					<p className="font-title text-3xl md:text-4xl">Drinks y Picoteo</p>
+					<p className="font-body font-semibold text-sm md:text-base mt-2">URBAN PLAZA. Calle 90 #11-13 Bogotá.</p>
+					<p className="font-body md:text-base md:leading-5 leading-4 text-sm  text-justify">
+						Quizimos hacer esta fiesta para que tengan la oportunidad de conocerse con toda la gente que irá al matrimonio, y para que puedan reencontrarse con
+						personas increibles que no ven hace años. Será un momento de disfrutar de algo rico para comer, y brindar juntos, llenos de ilusión, por el gran día
+						que se acerca. ¡Nos encantaría que nos acompañen! Por favor, confirmen su asistencia.
+					</p>
+					<p className="font-body font-semibold text-sm md:text-base mt-1">Be smart, be casual</p>
+				</div>
+				<div className="mt-1 z-10 flex flex-col gap-y-3 items-start">
+					<Link href={"/rsvp"} className="underline font-highlights text-sm md:text-base font-light">
+						Confirmar
+					</Link>
+					<Link target="__blank" href={"https://maps.app.goo.gl/2rrZMNUZ7HhrAUrAA"} className="underline font-highlights text-sm md:text-base font-light">
+						¿Cómo llegar?
+					</Link>
+				</div>
+				<div className="w-full h-8 relative my-4">
+					<Image src={"/assets/images/divider-3.svg"} fill alt="divider 1 " />
+				</div>
+
 				<div className="flex flex-col z-10">
-					<p className="uppercase font-body font-light text-xs md:text-sm">Sábado 2 de agosto</p>
+					<p className="uppercase font-body font-light text-xs md:text-sm">Sábado 2 de agosto, 3 pm</p>
 					<p className="font-title text-3xl md:text-4xl">Ceremonia y Celebración</p>
 					<p className="font-body font-semibold text-sm md:text-base mt-2">Finca Haras Primavera Sopó</p>
 					<p className="font-body md:text-base md:leading-5 leading-4 text-sm  text-justify">
-						Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over
-						2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-						consectetur, from a Lorem Ipsum passage
+						Los invitamos a celebrar nuestro matrimonio a las afueras de Bogotá, en la espectacular Finca Haras Primavera. La ceremonia arranca a las 3pm. Les
+						recordamos que el lugar está ubicado fuera de Bogotá, así que por favor planeen su viaje con anticipación. ¡Estamos emocionados de compartir este
+						día especial con ustedes! Por favor confirmen su asistencia.
 					</p>
 					<p className="font-body font-semibold text-sm md:text-base mt-1">Vestido Largo y Traje</p>
 				</div>
 
 				<div className="mt-1 flex flex-col gap-y-3 items-start">
 					<Link href={"/rsvp"} className="underline font-highlights text-sm md:text-base font-light">
-						confirmar
+						Confirmar
 					</Link>
 					<Link target="__blank" href={"https://maps.app.goo.gl/zY5o6rhzao82kfDG6"} className="underline font-highlights text-sm md:text-base font-light">
-						¿cómo llegar?
+						¿Cómo llegar?
 					</Link>
-					<button className="underline text-sm md:text-base font-highlights font-light">necesito transporte</button>
-				</div>
-				<div className="w-full h-8 relative my-4">
-					<Image src={"/assets/images/divider-3.svg"} fill alt="divider 1 " />
-				</div>
-				<div className="flex flex-col">
-					<p className="uppercase font-body font-light text-xs md:text-sm">Jueves 31 de agosto</p>
-					<p className="font-title text-3xl md:text-4xl">Drinks y Picoteo</p>
-					<p className="font-body font-semibold text-sm md:text-base mt-2">URBAN PLAZA. Calle 90 #11-13 Bogotá.</p>
-					<p className="font-body md:text-base md:leading-5 leading-4 text-sm  text-justify">
-						Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over
-						2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-						consectetur, from a Lorem Ipsum passage
-					</p>
-					<p className="font-body font-semibold text-sm md:text-base mt-1">Be smart, be casual</p>
 				</div>
 
-				<div className="mt-1 z-10 flex flex-col gap-y-3 items-start">
-					<Link href={"/rsvp"} className="underline font-highlights text-sm md:text-base font-light">
-						confirmar
-					</Link>
-					<Link target="__blank" href={"https://maps.app.goo.gl/2rrZMNUZ7HhrAUrAA"} className="underline font-highlights text-sm md:text-base font-light">
-						¿cómo llegar?
-					</Link>
-					<button className="underline font-highlights text-sm md:text-base font-light">necesito transporte</button>
-				</div>
-				<div className="h-[60svh] mt-[-80px] relative w-full ">
-					<Image src={"/assets/images/picoteo-1.png"} alt="detalles" fill className="object-contain" />
+				<div className="h-[70svh] mt-[-120px] relative w-full ">
+					<Image src={"/assets/images/wed.png"} alt="detalles" fill className="object-contain" />
 				</div>
 			</main>
 		</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,50 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import Menu from "./components/menu";
+import { useBreakpoint } from "./hooks/useGPTBreakpoint";
 
 export default function Home() {
+	const { isMd } = useBreakpoint();
 	const [openMenu, setOpenMenu] = useState<boolean>(false);
 
 	return (
 		<div id="scrolling-container" className="items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex h-[100svh] w-full flex-col gap-8 row-start-2 overflow-y-hidden items-center sm:items-between">
-				<p className="mx-5 relative font-title sm:text-4xl text-3xl md:text-5xl mt-5 z-10 whitespace-break-spaces text-foreground">
-					Álvaro &{"\n"}María Camila
-				</p>
-				<div className="h-[70svh] absolute right-0 bottom-0 max-w-[700px] mb-[-50px] w-[90svw] md:w-[80svw]">
-					<Image className="object-contain object-right-bottom z-[9]" src={"/assets/images/wed.png"} fill alt="cover"></Image>
+				<div className="flex flex-col w-full justify-start items-start">
+					<p className="mx-5 relative font-title sm:text-4xl text-3xl md:text-5xl mt-5 z-10 whitespace-break-spaces text-foreground">
+						Álvaro &{"\n"}María Camila
+					</p>
+					<p className="mx-5 relative font-title sm:text-4xl text-lg md:text-xl z-10 whitespace-break-spaces text-foreground">2 de agosto, 2025</p>
+					<div className="flex flex-col gap-y-2 pl-5 mt-5">
+						<Link className="text-sm md:text-base underline cursor-pointer font-highlights font-light" href={"/"}>
+							Inicio
+						</Link>
+						<Link className="text-sm md:text-base underline cursor-pointer font-highlights font-light" href={"/detalles"}>
+							Detalles
+						</Link>
+						<Link className="text-sm md:text-base underline cursor-pointer font-highlights font-light" href={"/rsvp"}>
+							RSVP
+						</Link>
+						<Link className="text-sm md:text-base underline cursor-pointer font-highlights font-light" href={"/colombia"}>
+							Viaja por Colombia
+						</Link>
+						<Link className="text-sm md:text-base underline cursor-pointer font-highlights font-light" href={"/bogota"}>
+							Estadía en Bogota
+						</Link>
+						<Link className="text-sm md:text-base underline cursor-pointer font-highlights font-light" href={"/regalos"}>
+							Lista de Regalos
+						</Link>
+					</div>
 				</div>
-				{/* <div className="flex justify-center w-full h-20">
-					<button onClick={() => setOpenMenu(true)} className="text-sm underline">
-						ver menú
-					</button>
-				</div> */}
+				<div className="md:h-[90svh] h-[60svh] w-full absolute right-0 bottom-0 md:w-4/5">
+					<Image
+						className="object-cover  md:object-contain object-right-bottom z-[9]"
+						src={isMd ? "/assets/images/wed-extended.png" : "/assets/images/wed-extended-mobile.png"}
+						fill
+						alt="cover"></Image>
+				</div>
 			</main>
 			<Menu open={openMenu} close={() => setOpenMenu(false)} />
 		</div>
