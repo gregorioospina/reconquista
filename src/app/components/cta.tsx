aaabfbd8b9179af4bee49d437b8b4eb5b79aba5d
gregorio_ospina <gregorio.ospina@rappi.com>
2025-02-25 17:23:17 -0500

    va

--- src/app/components/cta.tsx
+++ src/app/components/cta.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 const CTA = () => {
 	return (
-		<div className="fixed z-50 bottom-5 right-5 flex items-center">
+		<a
+			href="https://wa.me/+573332739911?text=%C2%A1Hola%21%20Tengo%20dudas%20sobre%20el%20matrimonio%20de%20%C3%81lvaro%20Rocafort%20y%20Maria%20Camila%20Vernaza."
+			className="fixed z-50 bottom-5 right-5 flex items-center">
 			<div className="rounded-full z-10 mr-[-10px] overflow-visible bg-rc-blue">
 				<Image src={"/assets/images/faq.svg"} height={35} width={35} alt="faq" />
 			</div>
 			<p className=" bg-rc-blue text-background px-3 font-highlights rounded-md">¿Tienes dudas?</p>
-		</div>
+		</a>
 	);
 };
 export default CTA;
